f616ef5c6faeb164975eb63485e37ffa082dfbaf
Mariana Monteiro <up202003480@fe.up.pt>
2022-05-03 12:27:10 +0100

    Jasmin and Ollir initial files

---
+++ src/pt/up/fe/comp/Jasmin/JasminEmitter.java
@@ -0,0 +1,15 @@
+package pt.up.fe.comp.Jasmin;
+
+import pt.up.fe.comp.jmm.jasmin.JasminBackend;
+import pt.up.fe.comp.jmm.jasmin.JasminResult;
+import pt.up.fe.comp.jmm.ollir.OllirResult;
+
+public class JasminEmitter implements JasminBackend {
+
+    @Override
+    public JasminResult toJasmin(OllirResult ollirResult) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+}
