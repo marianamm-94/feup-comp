2ac3ae87170c8545e061651fb0f5d19ef2a9bb65
Tea Madzarac <up202111384@fe.up.pt>
2022-05-09 23:27:44 +0000

    Replace AssignmentVisitor.java

--- src/pt/up/fe/comp/Visitor/AssignmentVisitor.java
+++ src/pt/up/fe/comp/Visitor/AssignmentVisitor.java
@@ -22,23 +22,33 @@ public class AssignmentVisitor extends PreorderJmmVisitor<JmmAnalyser, Boolean> 
 
         if(leftNode.getKind().equals("Identifier")) {
             left = Utils.getTypeVar(leftNode, symbolTableReport, parentMethod);
-        } else {
-            JmmNode arrayIdentifier = leftNode.getChildren().get(0);
-            left = Utils.getTypeVar(arrayIdentifier, symbolTableReport, parentMethod).split("\\[", 0)[0];
+        } else { //in case of an array
+            JmmNode array = leftNode.getChildren().get(0);
+            left = Utils.getTypeVar(array, symbolTableReport, parentMethod).split("\\[")[0];
         }
 
+        //types of right and left node do not match
+
+        //int i;
+        //i = false;
         if (rightNode.getChildren().size() == 0) {
             right = Utils.getTypeVar(rightNode, symbolTableReport, parentMethod);
             if (!right.equals(left)) {
                 symbolTableReport.newReport(rightNode, "\"" + rightNode + "\" and \"" + leftNode + "\" incompatible types");
             }
         }
+
+        //i = true && true
         else if(Utils.isBoolOp(rightNode.getKind()) && !left.equals("boolean")){
             symbolTableReport.newReport(rightNode, "\"" + rightNode + "\" and \"" + leftNode + "\" incompatible types");
         }
+
+        //we can only do math operations over numbers
         else if(Utils.isMathOp(rightNode.getKind()) && !left.equals("int") && !left.equals("int[]")) {
             symbolTableReport.newReport(rightNode, "\"" + rightNode + "\" and \"" + leftNode + "\" incompatible types");
         }
+
+        //if node is methhod check return type
         else if(rightNode.getKind().equals("FullStop")) {
             String returnType = Utils.getReturnType(rightNode, symbolTableReport);
             if (!returnType.equals("undefined") && !returnType.equals(left)) {
