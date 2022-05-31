f08dc2ec0efa20f6eb0941abb61eec86936ee8ea
sneinhnz <marcosw_ferreira@pm.me>
2022-05-31 11:35:48 +0100

    working on goto and conditions

--- src/pt/up/fe/comp/Jasmin/JasminLoops.java
+++ src/pt/up/fe/comp/Jasmin/JasminLoops.java
@@ -2,8 +2,39 @@ package pt.up.fe.comp.Jasmin;
 
 import org.specs.comp.ollir.*;
 
+import java.util.List;
+
 public class JasminLoops {
-    public static String gotoInstruction( GotoInstruction instruction){
+
+    public static String gotoInstruction(GotoInstruction instruction){
         return "goto " + instruction.getLabel() + "\n";
     }
+
+    public static String branchInstruction(CondBranchInstruction instruction, Method method){
+
+        StringBuilder jasminCode= new StringBuilder();
+        var varTable= method.getVarTable();
+
+        Element leftElement = instruction.getOperands().get(0);
+        Element leftRight = instruction.getOperands().get(1);
+
+
+
+
+        return jasminCode.toString();
+    }
+
+    public static String evaluateBranchCondition(String leftInstruction, String rightInstruction, OperationType operationType, String label){
+        StringBuilder jasminCode= new StringBuilder();
+
+        //TODO
+        if (operationType == OperationType.LTH) jasminCode.append();
+        else if (operationType == OperationType.ANDB) jasminCode.append();
+        else if (operationType == OperationType.NOTB) jasminCode.append();
+        else if (operationType == OperationType.GTE) jasminCode.append();
+
+
+        return jasminCode.toString();
+    }
+
 }
